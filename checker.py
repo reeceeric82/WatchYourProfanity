60d91d58339f93dec6cd57bdd1e9e665bc5253ee
Reece Russell <rerussell@protonmail.com>
2023-12-26 22:33:30 +0200

    Modified - Cleanup checker.py

--- checker.py
+++ checker.py
@@ -2,6 +2,7 @@ from resources import swearwords
 import re
 from difflib import SequenceMatcher
 
+
 def censor_text(text, banned_words):
     for word in banned_words:
         pattern = re.compile(r'\b' + re.escape(word) + r'\b', re.IGNORECASE)
@@ -18,7 +19,5 @@ def is_safe(text, banned_words):
     return True
 
 
-
 def check(word, blacklist):
     return SequenceMatcher(None, word, blacklist).ratio()
-
